f08d40d5489c4ac3a4100081a5d752da66235a19
Matthew Majcenic <matthew.majcenic@gmail.com>
2025-04-04 13:06:46 -0400

    Added database.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.get('/api/customers', cors(), (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
+    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
   ];
 
   res.json(customers);
@@ -16,4 +16,3 @@ app.get('/api/customers', cors(), (req, res) => {
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
-//Test
