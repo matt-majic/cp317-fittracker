a992c3091eaff4b4cb65cecf8093d186c8347ef8
Azure-Night-Sky <amelia.duda@gmail.com>
2025-04-05 19:38:29 -0400

    Update startPage.js
    checking to see where the problem is

--- client/src/startPage.js
+++ client/src/startPage.js
@@ -5,6 +5,7 @@ import styles from "./startPage.module.css";
 function startPage() {
   return (
     <main className={styles.div}>
+      <h2>Test</h2>
       <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e8d20912f4e1f3b5c8855fb0b71c77626ae65c9" alt="FITTracker" className={styles.logo} />
     </main>
   );
