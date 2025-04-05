da9e07dc61014af810f57fbfef5edfcf9b1921f7
Azure-Night-Sky <amelia.duda@gmail.com>
2025-04-05 17:38:45 -0400

    Update and rename start_page.js to startPage.js

--- client/src/startPage.js
+++ client/src/startPage.js
@@ -1,8 +1,8 @@
 "use client";
 import * as React from "react";
-import styles from "./start-page.module.css";
+import styles from "./startPage.module.css";
 
-function start_page() {
+function startPage() {
   return (
     <main className={styles.div}>
       <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e8d20912f4e1f3b5c8855fb0b71c77626ae65c9" alt="FITTracker" className={styles.logo} />
@@ -10,4 +10,4 @@ function start_page() {
   );
 }
 
-export default start_page;
+export default startPage;
