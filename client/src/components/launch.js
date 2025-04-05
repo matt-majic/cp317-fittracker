8d5cf03228db99aeda5d76507dfe02802af7c480
Azure-Night-Sky <amelia.duda@gmail.com>
2025-04-05 15:58:28 -0400

    Create launch.js
    copied from conversion

---
+++ client/src/components/launch.js
@@ -0,0 +1,13 @@
+"use client";
+import * as React from "react";
+import styles from "./InputDesign.module.css";
+
+function InputDesign() {
+  return (
+    <main className={styles.div}>
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e8d20912f4e1f3b5c8855fb0b71c77626ae65c9" alt="FITTracker" className={styles.logo} />
+    </main>
+  );
+}
+
+export default InputDesign;
