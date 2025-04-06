8a36318a8d5ca37391564d80d845d3c7213492b5
Azure-Night-Sky <amelia.duda@gmail.com>
2025-04-06 13:28:26 -0400

    Update and rename caloriePage.jss to caloriePage.js
    typos fixed

--- client/src/components/caloriePage.js
+++ client/src/components/caloriePage.js
@@ -1,5 +1,5 @@
 /*
-This is the jss for the page for the calorie tracker, which shows 
+This is the js for the page for the calorie tracker, which shows 
 a the daily breakdowns of calories in and calories out,
 diplayed against the weight goal. 
 */
