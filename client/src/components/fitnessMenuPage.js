b8febdacd39d8051cf2e795158a3fd72b8fa8e04
Azure-Night-Sky <amelia.duda@gmail.com>
2025-04-06 15:45:44 -0400

    empty function added

--- client/src/components/fitnessMenuPage.js
+++ client/src/components/fitnessMenuPage.js
@@ -3,3 +3,7 @@ js file for the workout plans & sessions page. It has 5 options:
 Explore Sessions, My Sessions, Explore Workout Plans, My Workout Plans, and Explore Trainers. 
 Pressing one of these options would lead to the corresponding page. 
 */
+
+function fitnessMenuPage() {
+  return <h2>These are some temporary words.</h2>;
+}
