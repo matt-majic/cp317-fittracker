f366b5208cb7dd1e7a374816fee105f244fef469
grxam <graemeg24@gmail.com>
2025-04-07 03:27:26 -0400

    fixed the startpage css and js files

--- client/src/components/startPage.js
+++ client/src/components/startPage.js
@@ -3,22 +3,20 @@ js for the first page of the app. It shows the app logo, as well as two
 buttons: one says "Sign In" and the other says "Sign Up". 
 */
 
-// Note: The following code could potentially not work correctly
 "use client";
-import * as React from "react";
-import styles from "./startPage.css";
-
-console.log(styles);
+import React from "react";
+import "./startPage.css"; // not using styles from modules
 
 function StartPage() {
   return (
-    <main className={styles.div}>
-      <h2>Test</h2>
+    <main className="start-page-container">
+      <h2 className="logo">FITTracker</h2>
       <img
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e8d20912f4e1f3b5c8855fb0b71c77626ae65c9"
-        alt="FITTracker"
-        className={styles.logo}
+        alt="FITTracker Logo"
+        className="logo"
       />
+      {/* You can add your buttons here if needed */}
     </main>
   );
 }
